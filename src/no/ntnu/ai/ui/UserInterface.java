19eff9a2ed2c6b609cbca59083d4f7cabe58af95
Jørgen Nordmoen <nordmoen90@gmail.com>
2012-11-05 21:08:25 +0100

    Added error handeling and a nice fault message to UI when parser is not specified or is null

--- src/no/ntnu/ai/ui/UserInterface.java
+++ src/no/ntnu/ai/ui/UserInterface.java
@@ -11,11 +11,15 @@ import no.ntnu.ai.file.parser.Parser;
 public class UserInterface {
 
 	public static List<String> parseNBC(List<String> options, Parser parser){
-		assert(parser != null);
+		if(parser == null){
+			throw new RuntimeException("Parser was null");
+		}
 		return null;
 	}
 	public static List<String> parseDTC(List<String> options, Parser parser){
-		assert(parser != null);
+		if(parser == null){
+			throw new RuntimeException("Parser was null");
+		}
 		return null;
 	}
 
@@ -50,17 +54,26 @@ public class UserInterface {
 			List<List<String>> options = parseCommandLine(args);
 			List<String> parsers = new ArrayList<String>();
 			Map<String, Parser> fileParsers = new HashMap<String, Parser>();
-			for(List<String> option : options){
-				if(option.get(0).equalsIgnoreCase("nbc")){
-					parsers.addAll(parseNBC(option, fileParsers.get(option.get(1))));
-				}else if(option.get(0).equalsIgnoreCase("dtc")){
-					parsers.addAll(parseDTC(option, fileParsers.get(option.get(1))));
-				}else if(option.get(0).equalsIgnoreCase("file")){
-					fileParsers.put(option.get(1), parseFilename(option));
-				}else{
-					System.err.println("Did not recoqnize the option: '" + 
-							option.get(0) + "'");
+			try{
+				for(List<String> option : options){
+					if(option.get(0).equalsIgnoreCase("nbc")){
+						parsers.addAll(parseNBC(option, fileParsers.get(option.get(1))));
+					}else if(option.get(0).equalsIgnoreCase("dtc")){
+						parsers.addAll(parseDTC(option, fileParsers.get(option.get(1))));
+					}else if(option.get(0).equalsIgnoreCase("file")){
+						fileParsers.put(option.get(1), parseFilename(option));
+					}else{
+						System.err.println("Did not recoqnize the option: '" + 
+								option.get(0) + "'");
+					}
 				}
+			}catch(RuntimeException as){
+				System.err.println("Could not find a file parser for the " +
+						"specified command line option. Each --classifier need " +
+						"to have a specified filereader object which must be" +
+						"specified before the --classifier option");
+				as.printStackTrace();
+				System.exit(-1);
 			}
 		}else{
 			System.out.println("Usage:");
