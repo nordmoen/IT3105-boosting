74b63e1272d6ebda0eb3ced88fe1e25523eefb0c
Jørgen Nordmoen <nordmoen90@gmail.com>
2012-11-05 12:20:08 +0100

    Added an outline to our command-line parsing

--- src/no/ntnu/ai/ui/UserInteface.java
+++ src/no/ntnu/ai/ui/UserInteface.java
@@ -1,13 +1,60 @@
 package no.ntnu.ai.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class UserInteface {
+
+	public static List<String> parseNBC(List<String> options){
+		return null;
+	}
+	public static List<String> parseDTC(List<String> options){
+		return null;
+	}
+
+	public static String parseFilename(List<String> options){
+		return null;
+	}
+
+	public static List<List<String>> parseCommandLine(String[] args){
+		List<List<String>> result = new ArrayList<List<String>>();
+		int i = -1;
+		for(int j = 0; j < args.length; j++){
+			String s = args[j];
+			if(s.equalsIgnoreCase("--classifier") || s.equalsIgnoreCase("-c")){
+				result.add(new ArrayList<String>());
+				i++;
+			}else if(s.equalsIgnoreCase("--filename") || s.equalsIgnoreCase("-f")){
+				result.add(new ArrayList<String>());
+				i++;
+				result.get(i).add("file");
+			}else{
+				result.get(i).add(s);
+			}
+		}
+		return result;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<List<String>> options = parseCommandLine(args);
+		List<String> parsers = new ArrayList<String>();
+		String fileParser;
+		for(List<String> option : options){
+			if(option.get(0).equalsIgnoreCase("nbc")){
+				parsers.addAll(parseNBC(option));
+			}else if(option.get(0).equalsIgnoreCase("dtc")){
+				parsers.addAll(parseDTC(option));
+			}else if(option.get(0).equalsIgnoreCase("file")){
+				fileParser = parseFilename(option);
+			}else{
+				System.err.println("Did not recoqnize the option: '" + 
+						option.get(0) + "'");
+			}
+		}
 	}
 
 }
