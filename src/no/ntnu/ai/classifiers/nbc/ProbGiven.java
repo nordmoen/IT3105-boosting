af2d4fb1ce251e2abf4e1d36d39851cc9bd3ee18
Jørgen Nordmoen <nordmoen90@gmail.com>
2012-11-15 12:53:59 +0100

    Added memoization to ProbGiven

--- src/no/ntnu/ai/classifiers/nbc/ProbGiven.java
+++ src/no/ntnu/ai/classifiers/nbc/ProbGiven.java
@@ -1,18 +1,22 @@
 package no.ntnu.ai.classifiers.nbc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import no.ntnu.ai.data.DataElement;
 
 public class ProbGiven<T, T2> {
 	private final List<DataElement<T, T2>> instances;
 	private final List<Double> weights;
+	private final Map<Integer, Map<T, Double>> mem;
 	private double wSum = 0.0;
 	
 	ProbGiven(){
 		this.instances = new ArrayList<DataElement<T, T2>>();
 		this.weights = new ArrayList<Double>();
+		mem = new HashMap<Integer, Map<T,Double>>();
 	}
 	
 	public void addClass(DataElement<T, T2> classi, double weight){
@@ -22,9 +26,19 @@ public class ProbGiven<T, T2> {
 	}
 	
 	public double calc(int attribute, T attVal){
+		if(!mem.containsKey(attribute)){
+			mem.put(attribute, new HashMap<T, Double>());
+		}
+		if(!mem.get(attribute).containsKey(attVal)){
+			mem.get(attribute).put(attVal, calcValue(attribute, attVal));
+		}
+		return mem.get(attribute).get(attVal);
+	}
+	
+	private double calcValue(int attr, T attVal){
 		double result = 0.0;
 		for(int i = 0; i < this.instances.size(); i++){
-			if(instances.get(i).get(attribute).equals(attVal)){
+			if(instances.get(i).get(attr).equals(attVal)){
 				result += this.weights.get(i);
 			}
 		}
